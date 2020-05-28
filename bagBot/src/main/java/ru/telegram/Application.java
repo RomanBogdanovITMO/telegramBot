d33d7588418809b53a591766fc6c364d5a5bbe00
RomanBogdanovITMO <quke@inbox.ru>
2020-05-28 21:36:27 +0300

    test-28.05.2020

--- bagBot/src/main/java/ru/telegram/Application.java
+++ bagBot/src/main/java/ru/telegram/Application.java
@@ -6,23 +6,12 @@ import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class Application {
 
-    /*public Application(ApplicationConfiguration myConfig) {
-        this.myConfig = myConfig;
-        //простые настройки для обхода блокировки в телеграме
-        System.getProperties().put(myConfig.getPROXY(), myConfig.getCHECK());
-        System.getProperties().put(myConfig.getSOCKS_HOST(), myConfig.getHOST());
-        System.getProperties().put(myConfig.getSOCKS_PORT(), myConfig.getPORT());
-
-    }*/
-
-
-
 
     public static void main(String[] args) {
 
-        System.getProperties().put("proxySet", "true");
+        /*System.getProperties().put("proxySet", "true");
         System.getProperties().put("socksProxyHost", "127.0.0.1");
-        System.getProperties().put("socksProxyPort", "9150");
+        System.getProperties().put("socksProxyPort", "9150");*/
 
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
